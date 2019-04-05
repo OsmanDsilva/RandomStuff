d496ea1a19d90651e11c4001bdb6a7751c8f131b
Osman Dsilva <christopher.osman10@gmail.com>
2019-04-05 18:39:06 +0530

    Updated

--- Web Dev/shoppingCart-w-MongoDB-master/shoppingCart-w-MongoDB-master/starter/app.js
+++ Web Dev/shoppingCart-w-MongoDB-master/shoppingCart-w-MongoDB-master/starter/app.js
@@ -1,11 +1,15 @@
 const bodyParser = require('body-parser');
-
+//const ejs=require('ejs');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const path = require('path')
 
 const express = require('express')
 const app = express();
+
+
+app.set('view engine','ejs');
+app.set('views','views');
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,7 +23,8 @@ app.use((req, res, next) => {
   //res.send("<h1>Page not found</h1>")
   // res.status(404).send("<h1>Page not found</h1>")
   console.log("404")
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  res.status(404).render('404',{pageTitle: 'Page not found'});  
 })
 
 app.listen(3001);
--- Web Dev/shoppingCart-w-MongoDB-master/shoppingCart-w-MongoDB-master/starter/routes/admin.js
+++ Web Dev/shoppingCart-w-MongoDB-master/shoppingCart-w-MongoDB-master/starter/routes/admin.js
@@ -4,13 +4,15 @@ const path = require('path')
 
 const router = express.Router();
 
+const products=[];
+
 router.get('/add-product', (req, res, next) => {
     //res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
     res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    console.log(req.body);  
     res.redirect('/');
 });
 
